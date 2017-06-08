dbda717c3b09a53dc8864110d1e37af784a0ccc7
frol <frol.mr@ya.ru>
2017-06-08 14:37:40 +0300

    code fixes, deploy scenario for travis

--- app/assets/javascripts/calculate_profit.js
+++ app/assets/javascripts/calculate_profit.js
@@ -7,10 +7,10 @@ $(document).ready(function() {
       dataType: 'json',
       data: { invest_sum: data },
       success: function(json) {
-        optimistic_profit = +json.optimistic
-        realistic_profit = +json.realistic
+        optimistic_profit = +json.optimistic;
+        realistic_profit = +json.realistic;
         $('#result').text("");
-        $('#result').append("<p>При вложении " + data + " ваша годовая доходность составит:</p>")
+        $('#result').append("<p>При вложении " + data + " ваша годовая доходность составит:</p>");
         $('#result').append("<p>Оптимистично: <strong>" + optimistic_profit.toFixed(2) + "</strong></p>");
         $('#result').append("<p>Реалистично: <strong>" + realistic_profit.toFixed(2) + "</strong>  (на основании истрических данных)</p>");
       }
